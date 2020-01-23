86808bcc8bd191f111d339a10ee876606443f95f
MarcosLaydner <marcoslaydnerseila@gmail.com>
2020-01-23 23:43:16 +0000

    Updates

---
+++ precache-manifest.fe0a686fd5000067e4daa05e08a86da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950141881e1b0aa03710dcd60b92ed8c",
+    "url": "/initiative_tracker/index.html"
+  },
+  {
+    "revision": "3a89f0e6805581cbfe29",
+    "url": "/initiative_tracker/static/js/2.32724a56.chunk.js"
+  },
+  {
+    "revision": "be3eb4cbad5a344e9603583a731df32d",
+    "url": "/initiative_tracker/static/js/2.32724a56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f2a89446635e409223a",
+    "url": "/initiative_tracker/static/js/main.92c98b40.chunk.js"
+  },
+  {
+    "revision": "f1d9d0dbb6c86a20f959",
+    "url": "/initiative_tracker/static/js/runtime-main.90b1cdf3.js"
+  }
+]);
