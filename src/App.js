29b4ef35b4853871bf515248e98da3b1e3b79086
MarcosLaydner <marcoslaydnerseila@gmail.com>
2020-01-23 01:43:52 +0000

    Added add, remove and sort functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React, {useState} from 'react';
+import './App.css'
 
+export default function App() {
+  const [characters, setCharacters] = useState([]);
+  const [newCharacter, setNewCharacter] = useState('');
+  const [idList, setIdList] = useState([]);  
 
-function App() {
+  function RemoveCharacter (id) {
+    const arrayCopy = characters.filter((row) => row.id !== id);
+    setCharacters(arrayCopy);
+  }
+  
+  function handleAdd () {
+    setCharacters([...characters, newCharacter])
+    setNewCharacter({name: '', init: '', ac: '', id: generateId()})
+  }
 
-const [characters, setChracters] = useState([]);
+  function SortCharactersByInit (characters) {
+    const arrayCopy = characters.slice(0);
+    arrayCopy.sort((a,b) => b.init - a.init);
+    setCharacters(arrayCopy)
+  }
 
+  function generateId() {
+    let id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+
+    while (idList.includes(id)) {
+      id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    }
+
+    idList.push(id)
+    setIdList(idList)
+    return id;
+  }
 
   return (
-    <div className="App">
-      <button className='add'>
-        Add
-      </button>
-    </div>
+    <body>
+      <table>
+
+        <thead>
+          <tr className='header'>
+            <th>Name</th>
+            <th>Init</th>
+            <th>AC</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        {characters.map(character => (
+            <tr key={character.id} className='contents'>
+              <td>{character.name}</td>
+              <td>{character.init}</td>
+              <td>{character.ac}</td>
+              <td>
+                <button onClick={() => RemoveCharacter(character.id)} >remove</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+        <tr>
+            <td><input type="text" value={newCharacter.name} placeholder="name" onChange={e => setNewCharacter({ ...newCharacter, name: e.target.value})}/></td>
+            <td><input type="text" value={newCharacter.init} placeholder="init" onChange={e => setNewCharacter({ ...newCharacter, init: e.target.value})}/></td>
+            <td><input type="text" value={newCharacter.ac} placeholder="ac" onChange={e => setNewCharacter({ ...newCharacter, ac: e.target.value})}/></td>
+            <td><button type='button' onClick={handleAdd}>Add</button></td>
+          </tr>
+        </tfoot>
+
+      </table>
+     
+      <div>
+        <button className='buttons' type='button' onClick={() => SortCharactersByInit(characters)}>Sort</button>
+        <button className='buttons'>clear all</button>
+      </div>
+
+    </body>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 ReactDOM.render(<App />, document.getElementById('root'));
